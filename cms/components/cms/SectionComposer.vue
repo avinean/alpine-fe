63a73cd8e3b90de6587b0b4b15a2b0d2fa4faf7f
Vladyslav Lubeznyy <vladyslavlubeznyy@MacBook-Pro.local>
2024-03-30 13:17:46 +0200

    add aspect ratio to images

--- cms/components/cms/SectionComposer.vue
+++ cms/components/cms/SectionComposer.vue
@@ -163,10 +163,10 @@ const menu = [
                 { label: 'Автоматична', value: 'auto' },
                 { label: '1:1', value: 1 },
                 { label: '2:1', value: '2/1' },
-                { label: '3:2', value: '3/2' },
-                { label: '4:3', value: '4/3' },
-                { label: '3:4', value: '3/4' },
-                { label: '2:3', value: '2/3' },
+                { label: '3:1', value: '3/1' },
+                { label: '4:1', value: '4/1' },
+                { label: '1:4', value: '1/4' },
+                { label: '1:3', value: '1/3' },
                 { label: '1:2', value: '1/2' },
               ]"
             />
